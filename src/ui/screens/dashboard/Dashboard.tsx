feae520e4e0a9f00d679dc9fb494154e4d680167
raphpay <raphpay.dev@gmail.com>
2025-05-07 10:02:38 +0400

    Fix: remove unused prop #5

--- src/ui/screens/dashboard/Dashboard.tsx
+++ src/ui/screens/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 export default function Dashboard() {
   return (
     <>
-      <Header isLoggedIn={true} />
+      <Header />
       <main className="min-h-screen bg-gradient-to-br from-blue-50 to-white px-4 py-20 pt-32 flex flex-col items-center justify-center">
         <div className="max-w-2xl text-center">Dashboard</div>
       </main>
